00b8e598fdf053e02dc5c7f4be9f6768e0221455
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-02-25 23:25:51 +0700

    feat: сохранение редактирования профиля

--- index.js
+++ index.js
@@ -7,17 +7,28 @@ const profileEditButton = content.querySelector('.profile__edit-button');
 
 const profileEditPopup = document.querySelector('.popup');
 const profileEditPopupCloseButton = profileEditPopup.querySelector('.popup__close');
-const profileEditForm = profileEditPopup.querySelector('.form__info');
+const profileEditForm = profileEditPopup.querySelector('.form');
 const profileEditFormNameInput = profileEditForm.querySelector('input[name=name]');
 const profileEditFormAboutInput = profileEditForm.querySelector('input[name=about]');
 
+function closeProfileEditPopup() {
+  profileEditPopup.classList.remove('popup_opened');
+}
 
-
-profileEditButton.addEventListener('click', (event) => {
+profileEditButton.addEventListener('click', (evt) => {
   profileEditFormNameInput.value = profileName.textContent;
   profileEditFormAboutInput.value = profileAbout.textContent;
 
   profileEditPopup.classList.add('popup_opened');
 });
 
-profileEditPopupCloseButton.addEventListener('click', (event) => profileEditPopup.classList.remove('popup_opened'));
+profileEditPopupCloseButton.addEventListener('click', closeProfileEditPopup);
+
+profileEditForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  profileName.textContent = profileEditFormNameInput.value;
+  profileAbout.textContent = profileEditFormAboutInput.value;
+
+  closeProfileEditPopup();
+});
