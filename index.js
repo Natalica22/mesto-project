520beef65c7422eddaaaf37c33fcb3a5a15a6b49
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-02-26 01:44:05 +0700

    feat: отображение карточек при загрузке

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const profileEditForm = profileEditPopup.querySelector('.form');
 const profileEditFormNameInput = profileEditForm.querySelector('input[name=name]');
 const profileEditFormAboutInput = profileEditForm.querySelector('input[name=about]');
 
+function createPlaceObject(name, link) {
+  return {
+    name: name,
+    link: link
+  }
+}
+
+const initialPlaces = [
+  createPlaceObject('Архыз', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'),
+  createPlaceObject('Челябинская область', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'),
+  createPlaceObject('Иваново', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'),
+  createPlaceObject('Камчатка', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'),
+  createPlaceObject('Холмогорский район', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'),
+  createPlaceObject('Байкал', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg')
+];
+
 function closeProfileEditPopup() {
   profileEditPopup.classList.remove('popup_opened');
 }
@@ -31,4 +47,16 @@ profileEditForm.addEventListener('submit', (evt) => {
   profileAbout.textContent = profileEditFormAboutInput.value;
 
   closeProfileEditPopup();
+});
+
+const placeTemplate = content.querySelector('#place').content;
+const placesSection = content.querySelector('.places');
+
+initialPlaces.forEach((place) => {
+  const placeCard = placeTemplate.querySelector('.place').cloneNode(true);
+  const placeCardImage = placeCard.querySelector('.place__image');
+  placeCardImage.src = place.link;
+  placeCardImage.alt = place.name;
+  placeCard.querySelector('.place__title').textContent = place.name;
+  placesSection.prepend(placeCard);
 });
