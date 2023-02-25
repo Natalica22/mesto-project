b0e3c5ad92dee72f3300f28184e1c56208f517df
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-02-25 22:46:59 +0700

    feat: добавление заполнения формы редактирования профиля текущими данными

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const profileEditPopup = document.querySelector('.popup');
 const profileEditPopupCloseButton = profileEditPopup.querySelector('.popup__close');
 
 
-profileEditButton.addEventListener('click', (event) => profileEditPopup.classList.add('popup_opened'));
+profileEditButton.addEventListener('click', (event) => {
+  const profileName = content.querySelector('.profile__name').textContent;
+  profileEditPopup.querySelector('.form__info input[name=name]').value = profileName;
+  const profileAbout = content.querySelector('.profile__about').textContent;
+  profileEditPopup.querySelector('.form__info input[name=about]').value = profileAbout;
+
+  profileEditPopup.classList.add('popup_opened');
+});
+
 profileEditPopupCloseButton.addEventListener('click', (event) => profileEditPopup.classList.remove('popup_opened'));
