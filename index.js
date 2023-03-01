a89bde836780af83cd8254008c5b628621f24a4b
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-03-01 23:13:03 +0700

    style: изменение структуры файла index.js

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const page = document.querySelector('.page');
 const content = page.querySelector('.content');
 
+//поиск текущих значений профиля
+const profileName = content.querySelector('.profile__name');
+const profileAbout = content.querySelector('.profile__about');
+
+// поиск popup для профиля
+const profileEditPopup = page.querySelector('#profile-popup');
+
+// поиск кнопки редактирования профиля
+const profileEditButton = content.querySelector('.profile__edit-button');
+
+//поиск формы и полей для профиля
+const profileEditForm = profileEditPopup.querySelector('.form');
+const profileEditFormNameInput = profileEditForm.querySelector('input[name=name]');
+const profileEditFormAboutInput = profileEditForm.querySelector('input[name=about]');
+
+// поиск popup места
+const placePopup = page.querySelector('#place-popup');
+
+//поиск кнопки открытия формы добавления места
+const placeButton = content.querySelector('.profile__add-place-button');
+
+//поиск формы создаения карточки
+const placeForm = placePopup.querySelector('.form');
+const placeFormNameInput = placeForm.querySelector('input[name=name]');
+const placeFormImageInput = placeForm.querySelector('input[name=imageUrl]');
+
+//поиск шаблона карточки места
+const placeTemplate = content.querySelector('#place').content;
+const placesSection = content.querySelector('.places');
+const imagePopup = page.querySelector('#popup-image');
+const imagePopupImage = imagePopup.querySelector('.popup__image');
+const imagePopupImageTitle = imagePopup.querySelector('.popup__image-title');
+
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
@@ -13,81 +46,6 @@ function closePopup(popup) {
 function addCloseListener(popup) {
   popup.querySelector('.popup__close').addEventListener('click', (evt) => closePopup(popup));
 }
-
-//поиск текущих значений профиля
-const profileName = content.querySelector('.profile__name');
-const profileAbout = content.querySelector('.profile__about');
-
-// поиск popup для профиля
-const profileEditPopup = page.querySelector('#profile-popup');
-addCloseListener(profileEditPopup);
-
-// поиск кнопки редактирования профиля
-const profileEditButton = content.querySelector('.profile__edit-button');
-
-//поиск формы и полей для профиля
-const profileEditForm = profileEditPopup.querySelector('.form');
-const profileEditFormNameInput = profileEditForm.querySelector('input[name=name]');
-const profileEditFormAboutInput = profileEditForm.querySelector('input[name=about]');
-
-//открытие редактирования профиля
-profileEditButton.addEventListener('click', (evt) => {
-  profileEditFormNameInput.value = profileName.textContent;
-  profileEditFormAboutInput.value = profileAbout.textContent;
-
-  openPopup(profileEditPopup);
-});
-
-//сохранение введеных данных в форму редактирования профиля
-profileEditForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-
-  profileName.textContent = profileEditFormNameInput.value;
-  profileAbout.textContent = profileEditFormAboutInput.value;
-
-  closePopup(profileEditPopup);
-});
-
-// поиск popup места
-const placePopup = page.querySelector('#place-popup');
-addCloseListener(placePopup);
-
-//поиск кнопки открытия формы добавления места
-const placeButton = content.querySelector('.profile__add-place-button');
-
-//поиск формы создаения карточки
-const placeForm = placePopup.querySelector('.form');
-const placeFormNameInput = placeForm.querySelector('input[name=name]');
-const placeFormImageInput = placeForm.querySelector('input[name=imageUrl]');
-
-//открытие формы создания места
-placeButton.addEventListener('click', (evt) => {
-  //очистка полей формы
-  placeFormNameInput.value = '';
-  placeFormImageInput.value = '';
-
-  openPopup(placePopup);
-});
-
-//добавление карточки
-placeForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-
-  addPlaceCard(createPlaceCard(
-    {
-      name: placeFormNameInput.value,
-      link: placeFormImageInput.value
-    }));
-
-  closePopup(placePopup);
-});
-
-//поиск шаблона карточки места
-const placeTemplate = content.querySelector('#place').content;
-const placesSection = content.querySelector('.places');
-const imagePopup = page.querySelector('#popup-image');
-const imagePopupImage = imagePopup.querySelector('.popup__image');
-const imagePopupImageTitle = imagePopup.querySelector('.popup__image-title');
 
 function openImagePopup(place) {
   imagePopupImage.src = place.link;
@@ -118,6 +76,50 @@ function createPlaceCard(place) {
 function addPlaceCard(placeCard) {
   placesSection.prepend(placeCard);
 }
+
+//открытие редактирования профиля
+profileEditButton.addEventListener('click', (evt) => {
+  profileEditFormNameInput.value = profileName.textContent;
+  profileEditFormAboutInput.value = profileAbout.textContent;
+
+  openPopup(profileEditPopup);
+});
+
+//сохранение введеных данных в форму редактирования профиля
+profileEditForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  profileName.textContent = profileEditFormNameInput.value;
+  profileAbout.textContent = profileEditFormAboutInput.value;
+
+  closePopup(profileEditPopup);
+});
+
+//открытие формы создания места
+placeButton.addEventListener('click', (evt) => {
+  //очистка полей формы
+  placeFormNameInput.value = '';
+  placeFormImageInput.value = '';
+
+  openPopup(placePopup);
+});
+
+//добавление карточки
+placeForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  addPlaceCard(createPlaceCard(
+    {
+      name: placeFormNameInput.value,
+      link: placeFormImageInput.value
+    }));
+
+  closePopup(placePopup);
+});
+
+addCloseListener(profileEditPopup);
+
+addCloseListener(placePopup);
 
 //закрытие popup места
 addCloseListener(imagePopup);
